2e77e103623043eca3e6c0f72a81fcfc4f5f300e
yashrajkumardy@gmail.com <yashrajkumardy@gmail.com>
2025-09-26 03:06:42 +0530

    updated path

--- src/main/java/com/agriculture/FarmOracle/Config/FirebaseConfig.java
+++ src/main/java/com/agriculture/FarmOracle/Config/FirebaseConfig.java
@@ -1,7 +1,8 @@
 package com.agriculture.FarmOracle.Config;
 
+import java.io.InputStream;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -15,11 +16,14 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() {
         try {
-            // Load the service account file from classpath (src/main/resources)
-            ClassPathResource resource = new ClassPathResource("firebase-service-account.json");
+            InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("firebase-service-account.json");
+
+            if (serviceAccount == null) {
+                throw new RuntimeException("firebase-service-account.json not found in resources folder");
+            }
 
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
